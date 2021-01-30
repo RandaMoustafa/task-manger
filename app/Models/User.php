d46cf72def2b46c6d837fe8d27b52ee8da3e01e9
RandaMoustafa <fairystory93@gmail.com>
2021-01-30 13:13:33 +0200

    [creating] project/tag/task controller and model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -18,6 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'phone',
         'email',
         'password',
     ];
@@ -40,4 +42,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function create(array $data)
+    {
+    }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password']= Hash::make($password);
+    }
+  /*  public function setNameAttribute($name)
+    {
+        $this->attributes['name']= 'Mr.' . $name ;
+    }*/
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,28 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Route::get('1/users',[\App\Http\Controllers\API\V1\UserController::class, 'index']);
+//Route::get('1/users/{user}',[\App\Http\Controllers\API\V1\UserController::class, 'show']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/*Route::namespace('App\\Http\\Controllers\\API\\V1\\') ->group(function(){
+    Route::prefix(1/users)->group(function (){
+        Route::as('api.users.v1.')->group(function (){
+            Route::get('/','UserController@index')->name('index');
+            Route::get('/{user}','UserController@show')->name('show');
+        });
+});
+});*/
+Route::group([
+    'namespace' => 'App\\Http\\Controllers\\API\\V1\\',
+    'prefix'=> '1/users',
+    'as'=> 'api.users.v1.'
+
+],function (){
+    Route::get('/','UserController@index')->name('index');
+    Route::get('/{user}','UserController@show')->name('show');
+    Route::post('/','UserController@store')->name('store');
+    Route::put('/{user}','UserController@update')->name('update');
+    Route::delete('/{user}','UserController@destroy')->name('destroy');
+
 });
